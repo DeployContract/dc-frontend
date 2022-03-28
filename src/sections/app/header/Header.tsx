ab8c4dce93927edad7c1d1085c2f5dbe9deef1db
AzerothA <telifesite@gmail.com>
2022-03-28 13:58:36 +0430

    Nav added

--- src/sections/app/header/Header.tsx
+++ src/sections/app/header/Header.tsx
@@ -1,21 +1,52 @@
 import React from "react";
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
-import { navWidth } from "./Navigation";
+import {
+    Box,
+    AppBar,
+    Toolbar,
+    Typography,
+    Icon,
+    IconButton,
+} from "@mui/material";
+import Navigation from "./Navigation";
+import MenuIcon from "@mui/icons-material/Menu";
+
+/**
+ * Default navigation options
+ */
+const DEFAULT_NAV_STATE = true;
+const DEFAULT_NAV_WIDTH = 300;
 
 function Header() {
+    const [navOpen, setNavOpen] = React.useState<boolean>(DEFAULT_NAV_STATE);
+
+    const changeNavOpen = () => setNavOpen(!navOpen);
+
     return (
         <Box>
             <AppBar
                 position="fixed"
-                sx={{
-                    width: { sm: `calc(100% - ${navWidth}px)` },
-                    ml: { sm: `${navWidth}px` },
-                }}
+                sx={
+                    navOpen
+                        ? {
+                              width: {
+                                  sm: `calc(100% - ${DEFAULT_NAV_WIDTH}px)`,
+                              },
+                              ml: { sm: `${DEFAULT_NAV_WIDTH}px` },
+                              transition: "all 0.2s",
+                          }
+                        : null
+                }
             >
                 <Toolbar>
+                    <IconButton onClick={changeNavOpen}>
+                        <Icon>
+                            <MenuIcon />
+                        </Icon>
+                    </IconButton>
                     <Typography variant="h5">App</Typography>
                 </Toolbar>
             </AppBar>
+            <Navigation navWidth={300} open={navOpen} onClose={changeNavOpen} />
         </Box>
     );
 }
