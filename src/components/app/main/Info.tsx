ad960bcd293ba7a907dfb78239827a0c6c28adc6
AzerothA <telifesite@gmail.com>
2022-04-14 22:39:04 +0430

    Impl update system

--- src/components/app/main/Info.tsx
+++ src/components/app/main/Info.tsx
@@ -6,11 +6,38 @@ import {
     CardContent,
     CardActions,
     Button,
-    CardMedia,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    DialogContentText,
 } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 import Icon from "../../common/Icon";
 
+declare var window: any;
+
 function Info() {
+    const [modalOpen, setModalOpen] = React.useState<boolean>(false);
+    const theme = useTheme();
+    const mobileScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+    const unRegister = () =>
+        navigator.serviceWorker.getRegistrations().then((res) =>
+            res.forEach((registration) => {
+                registration.unregister();
+            })
+        );
+
+    const updateHandler = () => {
+        caches.keys().then((keys) => {
+            for (let name of keys) caches.delete(name);
+        });
+    };
+
+    const changeModalOpen = () => setModalOpen(!modalOpen);
+
     return (
         <Stack
             direction="column"
@@ -25,9 +52,37 @@ function Info() {
                     <Typography variant="h5">Version: 0.0.1</Typography>
                 </CardContent>
                 <CardActions>
-                    <Button size="small">Update</Button>
+                    <Button size="small" onClick={changeModalOpen}>
+                        Update
+                    </Button>
                 </CardActions>
             </Card>
+            <Dialog
+                open={modalOpen}
+                fullScreen={mobileScreen}
+                onClose={changeModalOpen}
+            >
+                <DialogTitle>App Update</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure ? If yes Reload after upload
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={changeModalOpen}>
+                        Back
+                    </Button>
+                    <Button
+                        autoFocus
+                        onClick={() => {
+                            updateHandler();
+                            unRegister();
+                        }}
+                    >
+                        Update
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Stack>
     );
 }
