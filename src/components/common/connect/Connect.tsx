3a4ab5780bcbd5fab5bd8a5222a888ea56a19a18
AzerothA <telifesite@gmail.com>
2022-04-30 13:33:46 +0430

    Using connect hook

--- src/components/common/connect/Connect.tsx
+++ src/components/common/connect/Connect.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "@mui/material";
+import useConnect, { wallets } from "../../../library/src/hectorHook";
 
 function Connect() {
+    const [status, connect, getWallet] = useConnect(wallets.metamask());
+
+    useEffect(() => console.log(status));
+
     return (
-        <Button
-            variant="outlined"
-            sx={{ borderColor: "inherit", color: "inherit" }}
-        >
-            Connect
-        </Button>
+        <div>
+            <Button
+                variant="outlined"
+                sx={{ borderColor: "inherit", color: "inherit" }}
+                onClick={() => {
+                    connect();
+                }}
+            >
+                {status.isConnected ? "connected" : "NO connection"}
+            </Button>
+            <Button
+                variant="outlined"
+                sx={{ borderColor: "inherit", color: "inherit" }}
+                onClick={() => getWallet()}
+            >
+                Wallet
+            </Button>
+            {status.wallet ? status.wallet : "Wallet not found"}
+        </div>
     );
 }
 
