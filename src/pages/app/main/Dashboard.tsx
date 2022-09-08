bed3b2a823a334cdd35b898628d44d43a266da30
AzerothA <telifesite@gmail.com>
2022-09-08 10:21:48 +0430

    New Project (removed)

--- src/pages/app/main/Dashboard.tsx
+++ src/pages/app/main/Dashboard.tsx
@@ -24,22 +24,7 @@ interface Project {
 }
 
 function Dashboard() {
-    const [projects, setProjects] = React.useState<Project[]>([]);
-    const [dialogopen, setDialogOpen] = React.useState<boolean>(false);
-    const [newProject, setNewProject] = React.useState<Project>({
-        name: "",
-        description: "",
-    });
     const theme = useTheme();
-    const mobileScreen = useMediaQuery(theme.breakpoints.down("md"));
-
-    const updateProjects = (project: Project) =>
-        setProjects((proj) => [...proj, project]);
-
-    const toggleDialog = () => setDialogOpen((open) => !open);
-
-    const handleNewProject = (obj: object) =>
-        setNewProject((p) => Object.assign(p, obj));
 
     return (
         <Box>
@@ -50,90 +35,8 @@ function Dashboard() {
                 alignItems="center"
             >
                 <Typography variant="h5">Dashboard</Typography>
-                <Button variant="contained" onClick={toggleDialog}>
-                    New project
-                </Button>
+                <Button variant="contained">New Token</Button>
             </Stack>
-
-            <Grid
-                container
-                rowSpacing={3}
-                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                sx={{ mt: "20px" }}
-            >
-                {projects.map((x) => (
-                    <Grid item>
-                        <Card variant="outlined">
-                            <CardContent>
-                                <Stack direction="row" spacing={2}>
-                                    <Typography variant="h5">
-                                        {x.name}
-                                    </Typography>
-
-                                    <Chip label="Not Deployed" color="info" />
-                                </Stack>
-                                <Typography>{x.description}</Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button>Manage</Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-
-            <Dialog
-                open={dialogopen}
-                onClose={toggleDialog}
-                fullScreen={mobileScreen}
-            >
-                <DialogTitle>Create a new project</DialogTitle>
-                <DialogContent>
-                    <Stack spacing={3}>
-                        <TextField
-                            autoFocus
-                            id="name"
-                            label="Project Name"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            onChange={(event) =>
-                                handleNewProject({ name: event.target.value })
-                            }
-                        />
-
-                        <TextField
-                            id="description"
-                            label="Desciption"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            onChange={(event) =>
-                                handleNewProject({
-                                    description: event.target.value,
-                                })
-                            }
-                        />
-                    </Stack>
-                </DialogContent>
-
-                <DialogActions>
-                    <Button onClick={toggleDialog} color="error">
-                        Cancel
-                    </Button>
-                    <Button
-                        onClick={() => {
-                            updateProjects({
-                                name: newProject.name,
-                                description: newProject.description,
-                            });
-                            toggleDialog();
-                        }}
-                    >
-                        Create
-                    </Button>
-                </DialogActions>
-            </Dialog>
         </Box>
     );
 }
