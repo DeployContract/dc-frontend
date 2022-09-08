288a7c71e25988d0bf2b92f4ac03343136b8e8b6
AzerothA <telifesite@gmail.com>
2022-09-08 10:01:24 +0430

    New Update

--- src/pages/app/main/Info.tsx
+++ src/pages/app/main/Info.tsx
@@ -20,25 +20,7 @@ import Icon from "../../../components/icon/Icon";
 import { ThemeContext } from "../../../theme";
 
 function Info() {
-    const [modalOpen, setModalOpen] = React.useState<boolean>(false);
-    const theme = useTheme();
-    const mobileScreen = useMediaQuery(theme.breakpoints.down("md"));
     const { mode, setMode } = React.useContext(ThemeContext);
-
-    const unRegister = () =>
-        navigator.serviceWorker.getRegistrations().then((res) =>
-            res.forEach((registration) => {
-                registration.unregister();
-            })
-        );
-
-    const updateHandler = () => {
-        caches.keys().then(async (keys) => {
-            for (let name of keys) await caches.delete(name);
-        });
-    };
-
-    const changeModalOpen = () => setModalOpen(!modalOpen);
 
     const changeTheme = (
         _event: React.MouseEvent<HTMLElement>,
@@ -62,38 +44,7 @@ function Info() {
                     <Typography variant="h3">DeContract</Typography>
                     <Typography variant="h5">Version: 0.0.1</Typography>
                 </CardContent>
-                <CardActions>
-                    <Button size="small" onClick={changeModalOpen}>
-                        Update
-                    </Button>
-                </CardActions>
             </Card>
-            <Dialog
-                open={modalOpen}
-                fullScreen={mobileScreen}
-                onClose={changeModalOpen}
-            >
-                <DialogTitle>App Update</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Are you sure ? If yes Reload after upload
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button autoFocus onClick={changeModalOpen}>
-                        Back
-                    </Button>
-                    <Button
-                        autoFocus
-                        onClick={() => {
-                            updateHandler();
-                            unRegister();
-                        }}
-                    >
-                        Update
-                    </Button>
-                </DialogActions>
-            </Dialog>
             <ToggleButtonGroup
                 color="primary"
                 value={mode}
